66cf70bbfd73567c02bddda059f2e99d9d6d094d
Hans-J. Schmid <knock@myopendoor.de>
2022-10-29 06:23:32 +0200

    last two chapters (14/15) are a bit unfinished.

--- app/Singletons.hs
+++ app/Singletons.hs
@@ -4,10 +4,13 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Singletons where
 
@@ -20,81 +23,118 @@ data family Sing (a ∷ k)
 data SomeSing k where
   SomeSing ∷ Sing (a ∷ k) → SomeSing k
 
-class SingKind k where
-  type Demote k = r | r → k -- ! 1
-  toSing ∷ Demote k → SomeSing k
-  fromSing ∷ Sing (a ∷ k) → Demote k -- ! 2
-
-class SingI (a ∷ k) where
-  sing ∷ Sing a
-
+-- eliminator
 withSomeSing ∷ SomeSing k → (∀ (a ∷ k). Sing a → r) → r
 withSomeSing (SomeSing s) f = f s
 
--- # SingBool
+-- packaging together toSing and fromSing into a typeclass
+class SingKind k where
+  type Demote k = r | r → k
+  toSing ∷ Demote k → SomeSing k
+  fromSing ∷ Sing (a ∷ k) → Demote k -- type variable k is used both as a type and a kind
+
+------------------------
+-- Instances for Bool --
+------------------------
+
+-- Sing Bool
 data instance Sing (a ∷ Bool) where
   STrue ∷ Sing 'True
   SFalse ∷ Sing 'False
 
--- # SingITrue
-instance SingI 'True where
-  sing = STrue
-
--- # SingIFalse
-instance SingI 'False where
-  sing = SFalse
-
--- # SingKindBool
+-- Sing KindBool
 instance SingKind Bool where
   type Demote Bool = Bool
+  toSing ∷ Demote Bool → SomeSing Bool
   toSing True = SomeSing STrue
   toSing False = SomeSing SFalse
+  -- fromSing ∷ Sing a → Demote Bool
   fromSing STrue = True
   fromSing SFalse = False
 
--- # SingMaybe
+-- >>> withSomeSing (toSing True) fromSing
+-- True
+
+-- >>> withSomeSing (toSing False) fromSing
+-- False
+
+-- Because singletons are the unique inhabitant of their types, at the term-level they are isomorphic with ().
+-- Therefore, we expect to be able to get this unique inhabitant, in the same way we can always conjure a () out of thin air.
+
+class SingI (a ∷ k) where
+  sing ∷ Sing a
+
+---------------------------------
+-- Instances of SingI for Bool --
+---------------------------------
+
+-- SingI True
+instance SingI 'True where
+  sing ∷ Sing 'True
+  sing = STrue
+
+-- SingI False
+instance SingI 'False where
+  sing ∷ Sing 'False
+  sing = SFalse
+
+-- TODO (book seems to be wrong here)
+-- >>> :type sing @'True
+-- Expected a type, but 'True has kind `Bool'
+-- In the type 'True
+-- In the expression: sing @'True
+
+-- Sing Maybe
 data instance Sing (a ∷ Maybe k) where
   SJust ∷ Sing (a ∷ k) → Sing ('Just a)
   SNothing ∷ Sing 'Nothing
 
--- # SingINothing
+-- SingI Nothing
 instance SingI 'Nothing where
+  sing ∷ Sing 'Nothing
   sing = SNothing
 
--- # SingIJust
+-- SingI Just
 instance SingI a ⇒ SingI ('Just a) where
+  sing ∷ ∀ a1 (a2 ∷ a1). SingI a2 ⇒ Sing ('Just a2)
   sing = SJust sing
 
--- # SingKindMaybe
+-- Sing KindMaybe
 instance (k ~ Demote k, SingKind k) ⇒ SingKind (Maybe k) where
   type Demote (Maybe k) = Maybe k
+  toSing ∷ (k ~ Demote k, SingKind k) ⇒ Demote (Maybe k) → SomeSing (Maybe k)
   toSing (Just a) = withSomeSing (toSing a) $ SomeSing . SJust
   toSing Nothing = SomeSing SNothing
+  fromSing ∷ ∀ k' (a ∷ Maybe k'). (k' ~ Demote k', SingKind k') ⇒ Sing a → Demote (Maybe k')
   fromSing (SJust a) = Just $ fromSing a
   fromSing SNothing = Nothing
 
--- # SingList
+-- Sing List
 data instance Sing (a ∷ [k]) where
   SNil ∷ Sing '[]
   SCons ∷ Sing (h ∷ k) → Sing (t ∷ [k]) → Sing (h ': t)
 
--- # SingKindList
+-- SingKind List
 instance (k ~ Demote k, SingKind k) ⇒ SingKind [k] where
   type Demote [k] = [k]
+  toSing ∷ (k ~ Demote k, SingKind k) ⇒ Demote [k] → SomeSing [k]
   toSing [] = SomeSing SNil
   toSing (h : t) =
     withSomeSing (toSing h) $ \sh →
       withSomeSing (toSing t) $ \st →
         SomeSing $ SCons sh st
+  fromSing ∷ ∀ k' (a ∷ [k']). (k' ~ Demote k', SingKind k') ⇒ Sing a → Demote [k']
   fromSing SNil = []
   fromSing (SCons sh st) = fromSing sh : fromSing st
 
--- # SingINil
+-- SingI Nil
 instance SingI '[] where
+  sing ∷ Sing '[]
   sing = SNil
 
--- # SingICons
+-- SingI Cons
 instance (SingI h, SingI t) ⇒ SingI (h ': t) where
+  sing ∷ ∀ a (h' ∷ a) (t' ∷ [a]). (SingI h', SingI t') ⇒ Sing (h' : t')
   sing = SCons sing sing
 
 data Decision a = Proved a | Disproved (a → Void) -- ! 1
@@ -102,21 +142,24 @@ data Decision a = Proved a | Disproved (a → Void) -- ! 1
 class SDecide k where
   (%~) ∷ Sing (a ∷ k) → Sing (b ∷ k) → Decision (a :~: b)
 
--- # FreeSDecide
+-- Free SDecide
 instance (Eq (Demote k), SingKind k) ⇒ SDecide k where
+  (%~) ∷ ∀ k' (a ∷ k') (b ∷ k'). (Eq (Demote k'), SingKind k') ⇒ Sing a → Sing b → Decision (a :~: b)
   a %~ b =
     if fromSing a == fromSing b
       then Proved $ unsafeCoerce Refl
       else Disproved $ const undefined
 
--- # SDecideBool
+-- SDecide Bool
 instance SDecide Bool where
+  -- (%~) ∷ Sing a → Sing b → Decision (a :~: b)
   STrue %~ STrue = Proved Refl
   SFalse %~ SFalse = Proved Refl
   _ %~ _ = Disproved $ const undefined
 
--- # SDecideMaybe
+-- SDecide Maybe
 instance SDecide a ⇒ SDecide (Maybe a) where
+  (%~) ∷ ∀ a' (a1 ∷ Maybe a') (b ∷ Maybe a'). SDecide a' ⇒ Sing a1 → Sing b → Decision (a1 :~: b)
   SJust a %~ SJust b =
     case a %~ b of
       Proved Refl → Proved Refl
--- app/SingletonsTH.hs
+++ app/SingletonsTH.hs
@@ -1,26 +1,29 @@
-{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
 module SingletonsTH where
 
-import Data.Ord.Singletons -- (EQSym0, GTSym0, LTSym0, POrd (Compare), SOrd (sCompare), SOrdering (SEQ, SGT, SLT), ThenCmpSym0, sThenCmp)
-import Data.Singletons.TH -- (singletons)
+import Data.Ord.Singletons
+import Data.Singletons.TH (singletons)
 import Prelude.Singletons
 
--- ( FalseSym0,
---   FoldlSym0,
---   NilSym0,
---   PEq (type (==)),
---   PShow (ShowsPrec),
---   SBool (SFalse, STrue),
---   SEq ((%==)),
---   SFoldable (sFoldl),
---   SList (SNil),
---   SShow (sShowsPrec),
---   ShowStringSym0,
---   TrueSym0,
---   sShowString,
--- )
-
 -- # TimeOfDay
-singletons [d| data TimeOfDay = Morning | Afternoon | Evening | Night deriving (Eq, Ord, Show) |]
+singletons
+  [d|
+    data TimeOfDay
+      = Morning
+      | Afternoon
+      | Evening
+      | Night
+      deriving (Eq, Ord, Show)
+    |]
