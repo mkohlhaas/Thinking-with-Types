ab658d1f9d522142542b72edd63b87e089cb4d9e
Hans-J. Schmid <knock@myopendoor.de>
2022-11-04 03:21:00 +0100

    finishes ch. 10.

--- app/Defunc.hs
+++ app/Defunc.hs
@@ -4,20 +4,38 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
+--------------------------------------------
+-- Chapter 10: First Class Families (FCF) --
+--------------------------------------------
+
+-- 10.1 Defunctionalization
+-- 10.2 Type-Level Defunctionalization
+-- 10.3 Working with First Class Families
+-- 10.4 Ad-Hoc Polymorphism
+
 module Defunc where
 
 import Prelude hiding (fst)
 
+------------------------------
+-- 10.1 Defunctionalization --
+------------------------------
+
 fst ∷ (a, b) → a
 fst (a, _) = a
 
--- defunctionalizing `fst` by providing an equivalent label
+-- defunctionalizing `fst` by providing an EQUIVALENT LABEL
 newtype Fst a b = Fst (a, b)
+  deriving (Show)
 
--- implementing the actual evaluation function as a typeclass
+-- Implementing the actual evaluation function as a typeclass.
 -- The syntax `| l → t` is known as a FUNCTIONAL DEPENDENCY.
 -- It states that the type `t` is fully determined by the type `l`.
--- Here, l is the type of our defunctionalized label - Fst - and `t` is the return type of the evaluation.
+-- Here, `l` is the type of our defunctionalized label Fst and `t` is the return type of the evaluation.
+--     defunc label
+--         | return type
+--         | | functional dependency
+--         | |     |
 class Eval l t | l → t where
   eval ∷ l → t
 
@@ -25,45 +43,65 @@ instance Eval (Fst a b) a where
   eval ∷ Fst a b → a
   eval (Fst (a, _)) = a
 
--- >>> eval (Fst ("hello" , True))
+-- >>> Fst ("hello", True)
+-- Fst ("hello",True)
+
+-- >>> eval $ Fst ("hello", True)
 -- "hello"
 
--- Exercise 10.1-i: Defunctionalize `listToMaybe ∷ [a] → Maybe a`
+----------------------------------------------------
+-- Exercise 10.1-i: Defunctionalize `listToMaybe` --
+----------------------------------------------------
+
 listToMaybe ∷ [a] → Maybe a
 listToMaybe [] = Nothing
 listToMaybe (x : _) = Just x
 
-newtype ListToMaybe a = ListToMaybe [a]
+newtype ListToMaybe a = ListToMaybe [a] deriving (Show)
 
 instance Eval (ListToMaybe a) (Maybe a) where
   eval ∷ ListToMaybe a → Maybe a
   eval (ListToMaybe []) = Nothing
   eval (ListToMaybe (a : _)) = Just a
 
--- defunctionalizing higher-order functions
+-- >>> eval $ ListToMaybe []
+-- Nothing
+
+-- >>> eval $ ListToMaybe [1..5]
+-- Just 1
+
+----------------------------------------------
+-- Defunctionalizing Higher-Order Functions --
+----------------------------------------------
+
+-- >>> mapList fst [("hello", 1), ("world", 2)]
+-- ["hello","world"]
+
+-- >>> :type mapList fst [("hello", 1), ("world", 2)]
+-- mapList fst [("hello", 1), ("world", 2)] ∷ [String]
+
 mapList ∷ (a → b) → [a] → [b]
 mapList _ [] = []
 mapList f (a : as) = f a : mapList f as
 
--- >>> mapList fst [("hello", 1), ("world", 2)]
-
 data MapList a b = MapList (a → b) [a]
 
+-- 1st attempt
 -- instance Eval (MapList a b) [b] where
 --   eval ∷ MapList a b → [b]
 --   eval (MapList _ []) = []
 --   eval (MapList f (a : as)) = f a : eval (MapList f as)
 
+-- Fst is not evaluated
 -- >>> eval (MapList Fst [("hello", 1), ("world", 2)])
--- No instance for (Show (Fst String Integer))
---   arising from a use of `evalPrint'
--- In a stmt of an interactive GHCi command: evalPrint it_a7D5
+-- [Fst ("hello",1),Fst ("world",2)]
 
--- apart from the missing Show instance we should actually also eval Fst
+-- 2nd attempt
 instance (Eval b t) ⇒ Eval (MapList a b) [t] where
   eval ∷ Eval b t ⇒ MapList a b → [t]
   eval (MapList _ []) = []
   eval (MapList f (a : as)) = eval (f a) : eval (MapList f as)
 
+-- Fst is evaluated
 -- >>> eval (MapList Fst [("hello", 1), ("world", 2)])
 -- ["hello","world"]
--- app/FCTF.hs
+++ app/FCTF.hs
@@ -1,88 +1,131 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UnicodeSyntax #-}
 {-# LANGUAGE NoStarIsType #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
--- First Class Type Families
--- We can write a defunctionalized symbol whose type corresponds to the desired type-level function.
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+
+--------------------------------------------
+-- Chapter 10: First Class Families (FCF) --
+--------------------------------------------
+
+-- 10.1 Defunctionalization
+-- 10.2 Type-Level Defunctionalization
+-- 10.3 Working with First Class Families
+-- 10.4 Ad-Hoc Polymorphism
 
 module FCTF where
 
 import Data.Kind (Constraint, Type)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, listToMaybe)
 import GHC.TypeLits (type (+))
 
--- kind synonym which describes a type-level function which, when evaluated, will produce a type of kind A
+-----------------------------------------
+-- 10.2 Type-Level Defunctionalization --
+-----------------------------------------
+
+-- `Exp a` describes a type-level function
 type Exp a = a → Type
 
 -- >>> :kind Exp
 -- Exp ∷ Type → Type
 
--- Eval matches on `Exp a` mapping them to an A.
+-- >>> :kind Exp Int
+-- Exp Int ∷ Type
+
+-- Evaluation is performed via an open type family Eval.
 type family Eval (e ∷ Exp a) ∷ a
 
----------
--- Snd --
----------
+-------------------------------------
+-- Lifting `fst` to the Type Level --
+-------------------------------------
 
--- To lift `snd` to the type level, we write a data type whose kind mirrors the type of snd.
-data Snd ∷ (a, b) → Exp b
+-- >>> :type fst
+-- fst ∷ (a, b) → a
 
--- >>> :type snd
--- snd ∷ (a, b) → b
+-- >>> fst (1, "hello")
+-- 1
 
--- >>> :kind Snd
--- Snd ∷ (a, b) → b → Type
+-- Empty data types are used to write defunctionalized labels.
+data Fst ∷ (a, b) → Exp a
 
--- Eval Snd
-type instance Eval (Snd '(a, b)) = b
+-- >>> :kind Fst
+-- Fst ∷ (a, b) → a → Type
 
--- Note the promoted pair.
--- >>> :kind! Eval (Snd '(1, "hello"))
--- Eval (Snd '(1, "hello")) ∷ Symbol
+-- The types of `fst` and `Fst` look very similar.
+-- `Fst` just has `→ Type` appended.
+
+-- Fst doesn't have a data constructor.
+-- We are strictly operating at the type level.
+-- >>> Fst (1, "hello")
+-- Illegal term-level use of the type constructor `Fst' ...
+
+-- Eval Fst
+-- We need to "evaluate" it with an instance of the open type family `Eval`.
+type instance Eval (Fst '(a, b)) = a
+
+-- Evaluation can forced in the Repl with `:kind!`.
+-- >>> :kind! Eval (Fst '(1, "hello"))
+-- Eval (Fst '(1, "hello")) ∷ Natural
+-- = 1
+
+-- >>> :kind! Eval (Fst '("hello", 1))
+-- Eval (Fst '("hello", 1)) ∷ Symbol
 -- = "hello"
 
 ---------------
 -- FromMaybe --
 ---------------
 
--- Functions that perform PATTERN MATCHING can be lifted to the defunctionalized style by providing multiple type instances for Eval.
+-- >>> :type fromMaybe
+-- fromMaybe ∷ a → Maybe a → a
 
--- fromMaybe ∷ ∀ a. a → Maybe a → a
-
-def ∷ String
-def = fromMaybe "default" Nothing
-
--- >>> def
+-- >>> fromMaybe "default" Nothing
 -- "default"
 
+-- >>> fromMaybe "default" (Just "hello")
+-- "hello"
+
+-- defunctionalization
 data FromMaybe ∷ a → Maybe a → Exp a
 
+-- >>> :kind FromMaybe
+-- FromMaybe ∷ a → Maybe a → a → Type
+
+-- Functions like `fromMaybe` that perform PATTERN MATCHING can be lifted to the defunctionalized style by providing multiple type instances for Eval.
 -- pattern matching on `Maybe a`
 type instance Eval (FromMaybe a 'Nothing) = a
 
-type instance Eval (FromMaybe _1 ('Just a)) = a
+type instance Eval (FromMaybe _ ('Just a)) = a
 
--- >>> :kind! Eval(FromMaybe "nothing" 'Nothing)
--- Eval(FromMaybe "nothing" 'Nothing) ∷ Symbol
--- = "nothing"
+-- >>> :kind! Eval(FromMaybe "default" 'Nothing)
+-- Eval(FromMaybe "default" 'Nothing) ∷ Symbol
+-- = "default"
 
--- >>> :kind! Eval(FromMaybe "nothing" ('Just "just"))
--- Eval(FromMaybe "nothing" ('Just "just")) ∷ Symbol
--- = "just"
+-- >>> :kind! Eval(FromMaybe "default" ('Just "hello"))
+-- Eval(FromMaybe "default" ('Just "hello")) ∷ Symbol
+-- = "hello"
 
 -----------------
 -- ListToMaybe --
 -----------------
 
 -- Exercise 10.2-i: Defunctionalize listToMaybe at the type-level.
-listToMaybe ∷ [a] → Maybe a
-listToMaybe [] = Nothing
-listToMaybe (x : _) = Just x
 
+-- listToMaybe ∷ [a] → Maybe a
+-- listToMaybe [] = Nothing
+-- listToMaybe (x : _) = Just x
+
+-- >>> :type listToMaybe
+-- listToMaybe ∷ [a] → Maybe a
+
+-- defunctionalization
 data ListToMaybe ∷ [a] → Exp (Maybe a)
 
 type instance Eval (ListToMaybe '[]) = 'Nothing
@@ -93,9 +136,15 @@ type instance Eval (ListToMaybe (a ': _)) = 'Just a
 -- Eval (ListToMaybe '[]) ∷ Maybe a
 -- = 'Nothing
 
+-- >>> :kind! Eval (ListToMaybe '["hello", "world"])
+-- Eval (ListToMaybe '["hello", "world"]) ∷ Maybe Symbol
+-- = 'Just "hello"
+
 --------------
 -- Map List --
 --------------
+
+-- Defunctionalization at the type-level allows for higher-order functions!
 
 mapList ∷ (a → b) → [a] → [b]
 mapList _ [] = []
@@ -104,170 +153,32 @@ mapList f (a : as) = f a : mapList f as
 data MapList ∷ (a → Exp b) → [a] → Exp [b]
 
 -- Eval Nil
-type instance Eval (MapList f '[]) = '[]
+type instance Eval (MapList _ '[]) = '[]
 
 -- Eval Cons
 type instance Eval (MapList f (a ': as)) = Eval (f a) ': Eval (MapList f as)
 
+-- >>> :kind! Eval(MapList (FromMaybe 0) '[])
+-- Eval(MapList (FromMaybe 0) '[]) ∷ [Natural]
+-- = '[]
+
+-- side note: no tick needed for the list.
 -- >>> :kind! Eval(MapList (FromMaybe 0) ['Nothing, ('Just 1)])
--- Eval(MapList (FromMaybe 0) ['Nothing, ('Just 1)]) ∷ [Natural]
+-- Eval(MapList (FromMaybe 0) '[ 'Nothing, ('Just 1)]) ∷ [Natural]
 -- = '[0, 1]
 
--------------------------
--- Ad-Hoc Polymorphism --
--------------------------
+---------------------------------------------------------------------------
+-- Exercise 10.2-ii: Defunctionalize `foldr ∷ (a → b → b) → b → [a] → b` --
+---------------------------------------------------------------------------
 
----------
--- Map --
----------
+-- >>> :type foldr
+-- foldr ∷ Foldable t ⇒ (a → b → b) → b → t a → b
 
--- higher-order functions
+-- foldr for lists
+-- >>> :type foldr @[]
+-- foldr @[] ∷ (a → b → b) → b → [a] → b
 
--- generalizing MapList
-data Map ∷ (a → Exp b) → f a → Exp (f b)
-
--- Because type families are capable of discriminating on types, we can write several instances of Eval for Map!
-
---------------------
--- Map List again --
---------------------
-
-type instance Eval (Map f '[]) = '[]
-
-type instance Eval (Map f (a ': as)) = Eval (f a) ': Eval (Map f as)
-
--- >>> :kind! Eval(Map (FromMaybe 0) ['Nothing, ('Just 1)])
--- Eval(Map (FromMaybe 0) ['Nothing, ('Just 1)]) ∷ [Natural]
--- = '[0, 1]
-
----------
--- Sum --
----------
-
--- a littler helper function
-data Sum ∷ a → a → Exp a
-
-type instance Eval (Sum a b) = a + b
-
--- >>> :kind! Eval (Sum 2 3)
--- Eval (Sum 2 3) :: Natural
--- = 5
-
----------------
--- Map Maybe --
----------------
-
-type instance Eval (Map f 'Nothing) = 'Nothing
-
-type instance Eval (Map f ('Just a)) = 'Just (Eval (f a))
-
--- >>> :kind! Eval(Map (Sum 3) 'Nothing)
--- Eval(Map (Sum 3) 'Nothing) :: Maybe Natural
--- = 'Nothing
-
--- >>> :kind! Eval(Map (Sum 3) ('Just 5))
--- Eval(Map (Sum 3) ('Just 5)) :: Maybe Natural
--- = 'Just 8
-
-----------------
--- Map Either --
-----------------
-
-type instance Eval (Map f ('Left a)) = 'Left a
-
-type instance Eval (Map f ('Right b)) = 'Right (Eval (f b))
-
--- >>> :kind! Eval(Map (Sum 3) ('Left 5))
--- Eval(Map (Sum 3) ('Left 5)) :: Either Natural Natural
--- = 'Left 5
-
--- >>> :kind! Eval(Map (Sum 3) ('Right 5))
--- Eval(Map (Sum 3) ('Right 5)) :: Either a Natural
--- = 'Right 8
-
----------------
--- Map Tuple --
----------------
-
-type instance Eval (Map f '(a, b)) = '(a, Eval (f b))
-
--- >>> :kind! Eval(Map (Sum 3) '(4, 5))
--- Eval(Map (Sum 3) '(4, 5)) :: (Natural, Natural)
--- = '(4, 8)
-
-----------
--- (++) --
-----------
-
--- can be used to append types
-data (++) ∷ [a] → [a] → Exp [a]
-
-type instance Eval ((++) '[] ys) = ys
-
-type instance Eval ((++) (x ': xs) ys) = x ': Eval ((++) xs ys)
-
--- >>> :kind! Eval('[Int, Int, Int] ++ '[Bool, Bool, Bool])
--- Eval('[Int, Int, Int] ++ '[Bool, Bool, Bool]) :: [Type]
--- = '[Int, Int, Int, Bool, Bool, Bool]
-
--------------
--- Mappend --
--------------
-
--- semigroup operation Mappend to append Constraints
-data Mappend ∷ a → a → Exp a
-
--- pair
-type instance Eval (Mappend '() '()) = '()
-
--- Constraint
-type instance Eval (Mappend (a ∷ Constraint) (b ∷ Constraint)) = (a, b)
-
--- list
-type instance Eval (Mappend (a ∷ [k]) (b ∷ [k])) = Eval (a ++ b)
-
--- appending types
--- >>> :kind! Eval(Mappend '[Int, Int, Int] '[Bool, Bool, Bool])
--- Eval(Mappend '[Int, Int, Int] '[Bool, Bool, Bool]) :: [Type]
--- = '[Int, Int, Int, Bool, Bool, Bool]
-
--- appending Constraints
--- >>> :kind! Eval(Mappend (Eq Int) (Eq Bool))
--- Eval(Mappend (Eq Int) (Eq Bool)) :: Constraint
--- = (Eq Int, Eq Bool)
-
-------------
--- Mempty --
-------------
-
--- Type families are not allowed to discriminate on their return type.
--- "Data type has non-* return kind ‘k’"
--- data Mempty ∷ k
-
--- making the "type application" explicit
-data Mempty ∷ k → Exp k
-
--- Given a type of any monoidal kind K, Mempty will give back the monoidal identity for that kind.
-
--- empty pair
-type instance Eval (Mempty '()) = '()
-
--- empty Constraint
-type instance Eval (Mempty (c ∷ Constraint)) = (() ∷ Constraint)
-
--- empty list
-type instance Eval (Mempty (l ∷ [k])) = '[]
-
--- Mempty acts more like an annihilator
--- >>> :kind! Eval(Mappend (Eq Int) (Eval (Mempty (Eq Bool))))
--- Eval(Mappend (Eq Int) (Eval (Mempty (Eq Bool)))) :: Constraint
--- = (Eq Int, () :: Constraint)
-
------------
--- Foldr --
------------
-
--- Exercise 10.2-ii: Defunctionalize `foldr ∷ (a → b → b) → b → [a] → b`.
+-- defunctionalization
 data Foldr ∷ (a → b → Exp b) → b → [a] → Exp b
 
 -- pattern-matching on empty list
@@ -277,12 +188,12 @@ type instance Eval (Foldr _ b '[]) = b
 type instance Eval (Foldr f b (a ': as)) = Eval (f a (Eval (Foldr f b as)))
 
 -- >>> :kind! Eval(Foldr Sum 0 '[1, 2, 3, 4, 5])
--- Eval(Foldr Sum 0 '[1, 2, 3, 4, 5]) :: Natural
+-- Eval(Foldr Sum 0 '[1, 2, 3, 4, 5]) ∷ Natural
 -- = 15
 
----------------------------------------
--- Working with First Class Families --
----------------------------------------
+--------------------------------------------
+-- 10.3 Working with First Class Families --
+--------------------------------------------
 
 ----------
 -- Pure --
@@ -292,15 +203,19 @@ data Pure ∷ a → Exp a
 
 type instance Eval (Pure x) = x
 
+-- >>> :kind! Eval(Pure 5)
+-- Eval(Pure 5) ∷ Natural
+-- = 5
+
 -- >>> :kind! Eval(Pure "hello")
--- Eval(Pure "hello") :: Symbol
+-- Eval(Pure "hello") ∷ Symbol
 -- = "hello"
 
-------------------
--- (=<<) - bind --
-------------------
+----------------------------------------------
+-- (=<<) - bind with arguments interchanged --
+----------------------------------------------
 
--- First class families (FCF) form a monad at the type-level!
+-- First class families (FCFs) form a monad at the type-level!!
 
 data (=<<) ∷ (a → Exp b) → Exp a → Exp b
 
@@ -308,15 +223,15 @@ type instance Eval (k =<< e) = Eval (k (Eval e))
 
 infixr 0 =<<
 
---------------------
+---------------------
 -- (<=<) - Kleisli --
---------------------
+---------------------
 
 {- ORMOLU_DISABLE -}
 
 -- We can compose FCFs in terms of their Kleisli composition.
 -- Traditionally the fish operator (<=<) is used to represent this combinator in Haskell.
--- We are unable to use the more familiar period operator at the type-level, as it conflicts with the syntax of the forall quantifier.
+-- We are unable to use the more familiar period operator at the type-level, as it conflicts with the syntax of the ∀ quantifier.
 
 -- fish operator
 data (<=<) ∷ (b → Exp c) → (a → Exp b) → a → Exp c
@@ -325,21 +240,24 @@ type instance Eval ((f <=< g) x) = Eval (f (Eval (g x)))
 
 infixr 1 <=<
 
--- >>> type Snd2 = Snd <=< Snd
--- >>> :kind! Eval(Snd2 '(1 ,'(2 , 3)))
--- Eval(Snd2 '(1 ,'(2 , 3))) :: Natural
--- = 3
+-- >>> type Fst2 = Fst <=< Fst
+-- >>> :kind! Eval(Fst2 '( '(2 , 3), 1))
+-- Eval(Fst2 '( '(2 , 3), 1)) :: Natural
+-- = 2
 
--- At the type-level (<=<) acts like regular function composition (.),
--- while (=<<) behaves like function application ($).
+-- At the type-level (<=<) acts like regular function composition (.), while (=<<) behaves like function application ($).
+
+-- Remember:
+-- fish operator (<=<) ≅ function composition (.)
+-- reverse bind  (=<<) ≅ function application ($)
 
 -- >>> :kind! Eval(Snd <=< FromMaybe '(0 , 1) =<< Pure(Nothing))
--- Eval(Snd <=< FromMaybe '(0 , 1) =<< Pure(Nothing)) :: Natural
+-- Eval(Snd ⇐< FromMaybe '(0 , 1) =<< Pure(Nothing)) ∷ Natural
 -- = 1
 
--- >>> :kind! Eval(Snd <=< FromMaybe '(0 , 0) =<< Pure(Just '(2 , 3)))
--- Eval(Snd <=< FromMaybe '(0 , 0) =<< Pure(Just '(2 , 3))) :: Natural
--- = 3
+-- >>> :kind! Eval(Fst <=< FromMaybe '(0 , 0) =<< Pure(Just '(2 , 3)))
+-- Eval(Fst <=< FromMaybe '(0 , 0) =<< Pure(Just '(2 , 3))) :: Natural
+-- = 2
 
 {- ORMOLU_ENABLE -}
 
@@ -361,24 +279,24 @@ type family TyEqImpl (a ∷ k) (b ∷ k) ∷ Bool where
   TyEqImpl a b = 'False
 
 -- >>> :kind! Eval(TyEq Int Int)
--- Eval(TyEq Int Int) :: Bool
+-- Eval(TyEq Int Int) ∷ Bool
 -- = 'True
 
 -- >>> :kind! Eval(TyEq Int String)
--- Eval(TyEq Int String) :: Bool
+-- Eval(TyEq Int String) ∷ Bool
 -- = 'False
 
---------------
--- Collapse --
---------------
+-----------------
+-- Constraints --
+-----------------
 
--- collapsing lists of CONSTRAINTs
+-- Collapsing/Conjunction lists of constraints.
 
-data Collapse ∷ [Constraint] → Exp Constraint
+data Constraints ∷ [Constraint] → Exp Constraint
 
-type instance Eval (Collapse '[]) = (() ∷ Constraint)
+type instance Eval (Constraints '[]) = (() ∷ Constraint)
 
-type instance Eval (Collapse (a ': as)) = (a, Eval (Collapse as))
+type instance Eval (Constraints (a ': as)) = (a, Eval (Constraints as))
 
 -----------
 -- Pure1 --
@@ -390,19 +308,19 @@ data Pure1 ∷ (a → b) → a → Exp b
 type instance Eval (Pure1 f x) = f x
 
 -- >>> :kind! Eval(Pure1 Eq Int)
--- Eval(Pure1 Eq Int) :: Constraint
+-- Eval(Pure1 Eq Int) ∷ Constraint
 -- = Eq Int
 
 ---------
 -- All --
 ---------
 
-type All (c ∷ k → Constraint) (ts ∷ [k]) = Collapse =<< MapList (Pure1 c) ts
+type All (c ∷ k → Constraint) (ts ∷ [k]) = Constraints =<< MapList (Pure1 c) ts
 
 -- Much nicer implementation of All than chapter 5's.
 -- >>> :kind! Eval(All Eq '[Int , Bool])
--- Eval(All Eq '[Int , Bool]) :: Constraint
--- = (Eq Int, (Eq Bool, () :: Constraint))
+-- Eval(All Eq '[Int , Bool]) ∷ Constraint
+-- = (Eq Int, (Eq Bool, () ∷ Constraint))
 
 -----------
 -- Pure2 --
@@ -413,7 +331,7 @@ data Pure2 ∷ (a → b → c) → a → b → Exp c
 type instance Eval (Pure2 f x y) = f x y
 
 -- >>> :kind! Eval(Pure2 Sum 1 2)
--- Eval(Pure2 Sum 1 2) :: Natural -> Type
+-- Eval(Pure2 Sum 1 2) ∷ Natural → Type
 -- = Sum 1 2
 
 -----------
@@ -450,12 +368,163 @@ data Flip ∷ (a → b → Exp c) → b → a → Exp c
 
 type instance Eval (Flip f y x) = Eval (f x y)
 
-{-
+------------------------------
+-- 10.4 Ad-Hoc Polymorphism --
+------------------------------
 
--- ???
-data Fmap ∷ (a → b) → Exp a → Exp b
+---------
+-- Map --
+---------
 
-type instance Eval (Fmap f fa) = f (Eval fa)
+-- higher-order functions
 
--}
+-- >>> :type fmap
+-- fmap :: Functor f => (a -> b) -> f a -> f b
+
+-- defunctionalization
+data Map ∷ (a → Exp b) → f a → Exp (f b)
+
+-- Because type families are capable of discriminating on types, we can write several instances of Eval for Map!
+-- E.g. Map List, Map Maybe, Map Either, Map Tuple ...
+
+--------------------
+-- Map List again --
+--------------------
+
+type instance Eval (Map f '[]) = '[]
+
+type instance Eval (Map f (a ': as)) = Eval (f a) ': Eval (Map f as)
+
+-- >>> :kind! Eval(Map (FromMaybe 0) ['Nothing, ('Just 1)])
+-- Eval(Map (FromMaybe 0) ['Nothing, ('Just 1)]) ∷ [Natural]
+-- = '[0, 1]
+
+----------
+-- (:+) --
+----------
+
+-- >>> :type (+)
+-- (+) :: Num a => a -> a -> a
+
+-- a littler helper function
+data (:+) ∷ a → a → Exp a
+
+type instance Eval ((:+) a b) = a + b
+
+-- >>> :kind! Eval ((+) 2 3)
+-- Eval ((:+) 2 3) :: Natural
+-- = 5
+
+---------------
+-- Map Maybe --
+---------------
+
+type instance Eval (Map f 'Nothing) = 'Nothing
+
+type instance Eval (Map f ('Just a)) = 'Just (Eval (f a))
+
+-- >>> :kind! Eval(Map ((:+) 3) 'Nothing)
+-- Eval(Map ((:+) 3) 'Nothing) :: Maybe Natural
+-- = 'Nothing
+
+-- >>> :kind! Eval(Map ((:+) 3) ('Just 5))
+-- Eval(Map ((:+) 3) ('Just 5)) :: Maybe Natural
+-- = 'Just 8
+
+----------------
+-- Map Either --
+----------------
+
+type instance Eval (Map f ('Left a)) = 'Left a
+
+type instance Eval (Map f ('Right b)) = 'Right (Eval (f b))
+
+-- >>> :kind! Eval(Map ((:+) 3) ('Left 5))
+-- Eval(Map ((:+) 3) ('Left 5)) :: Either Natural Natural
+-- = 'Left 5
+
+-- >>> :kind! Eval(Map ((:+) 3) ('Right 5))
+-- Eval(Map ((:+) 3) ('Right 5)) :: Either a Natural
+-- = 'Right 8
+
+---------------
+-- Map Tuple --
+---------------
+
+type instance Eval (Map f '(a, b)) = '(a, Eval (f b))
+
+-- >>> :kind! Eval(Map ((:+) 3) '(4, 5))
+-- Eval(Map ((:+) 3) '(4, 5)) :: (Natural, Natural)
+-- = '(4, 8)
+
+----------
+-- (++) --
+----------
+
+-- >>> :type (++)
+-- (++) :: [a] -> [a] -> [a]
+
+-- can be used to append types
+data (++) ∷ [a] → [a] → Exp [a]
+
+type instance Eval ((++) '[] ys) = ys
+
+type instance Eval ((++) (x ': xs) ys) = x ': Eval ((++) xs ys)
+
+-- >>> :kind! Eval('[Int, Int, Int] ++ '[Bool, Bool, Bool])
+-- Eval('[Int, Int, Int] ++ '[Bool, Bool, Bool]) ∷ [Type]
+-- = '[Int, Int, Int, Bool, Bool, Bool]
+
+-------------
+-- Mappend --
+-------------
+
+-- semigroup operation Mappend to append Constraints
+data Mappend ∷ a → a → Exp a
+
+-- pair
+type instance Eval (Mappend '() '()) = '()
+
+-- Constraint
+type instance Eval (Mappend (a ∷ Constraint) (b ∷ Constraint)) = (a, b)
+
+-- list
+type instance Eval (Mappend (a ∷ [k]) (b ∷ [k])) = Eval (a ++ b)
+
+-- appending types
+-- >>> :kind! Eval(Mappend '[Int, Int, Int] '[Bool, Bool, Bool])
+-- Eval(Mappend '[Int, Int, Int] '[Bool, Bool, Bool]) ∷ [Type]
+-- = '[Int, Int, Int, Bool, Bool, Bool]
+
+-- appending Constraints
+-- >>> :kind! Eval(Mappend (Eq Int) (Eq Bool))
+-- Eval(Mappend (Eq Int) (Eq Bool)) ∷ Constraint
+-- = (Eq Int, Eq Bool)
+
+------------
+-- Mempty --
+------------
+
+-- Type families are not allowed to discriminate on their return type.
+-- "Data type has non-* return kind ‘k’"
+-- data Mempty ∷ k
+
+-- making the "type application" explicit
+data Mempty ∷ k → Exp k
+
+-- Given a type of any monoidal kind K, Mempty will give back the monoidal identity for that kind.
+
+-- empty pair
+type instance Eval (Mempty '()) = '()
+
+-- empty Constraint
+type instance Eval (Mempty (c ∷ Constraint)) = (() ∷ Constraint)
+
+-- empty list
+type instance Eval (Mempty (l ∷ [k])) = '[]
+
+-- Mempty acts more like an annihilator
+-- >>> :kind! Eval(Mappend (Eq Int) (Eval (Mempty (Eq Bool))))
+-- Eval(Mappend (Eq Int) (Eval (Mempty (Eq Bool)))) ∷ Constraint
+-- = (Eq Int, () ∷ Constraint)
 
